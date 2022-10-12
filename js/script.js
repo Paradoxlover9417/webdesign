dc09a16c4a5d90568c553ac205bfe6d31a69a894
Paradoxlover9417 <tksgk853@gmail.com>
2022-10-12 16:15:12 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$(function() {
+    $('.main .background').slick({
+    dots: false,
+    infinite: true,
+    speed: 1500,
+    fade: true,
+    cssEase: 'linear',
+    arrows: 'false',
+    autoplay: 'true'
+    });
+});
+    
+$(function() {
+    $('.animate').scrolla({
+        mobile: true,
+        once: false
+    })
+})
